1d3ff61849d299232054d3c56acbf866ff2bc002
heeeete <mkoui@naver.com>
2023-05-20 14:25:51 +0900

    commit

--- react.js
+++ react.js
@@ -1,49 +1,22 @@
 const root = document.getElementById("root");
-
-function Container() {
-	const [minutes, setmin] = React.useState();
-	const [hour, sethour] = React.useState();
-	const onChangeMin = (event) => {
-		setmin(event.target.value);
-		sethour(event.target.value / 60);
-	};
-	const onChangeHour = (event) => {
-		sethour(event.target.value);
-		setmin(event.target.value * 60);
-	};
-	const reset = () => {
-		setmin();
-		sethour();
-	};
+const Btn = ({ text, onChangeText }) => {
+	console.log(text);
+	return (
+		<button onClick={onChangeText} className="Btn">
+			{text}
+		</button>
+	);
+};
+const MemorizeBtn = React.memo(Btn);
+const App = () => {
+	const [btn, setbtn] = React.useState("asd");
+	const onChangeText = () => setbtn("qqweasdasdasdas");
 	return (
 		<div>
-			<h1>Super Converter!!</h1>
-			<div>
-				<label htmlFor="min utes">Minutes</label>
-				<input
-					value={minutes}
-					type="number"
-					placeholder="Minutes"
-					id="minutes"
-					onChange={onChangeMin}
-					// disabled={filp === true}
-				/>
-				<h4>you want Convert {minutes}</h4>
-			</div>
-			<div>
-				<label htmlFor="hours">Hours</label>
-				<input
-					value={hour}
-					type="number"
-					placeholder="Hours"
-					id="hours"
-					onChange={onChangeHour}
-					// disabled={filp === false}
-				/>
-			</div>
-			<button onClick={reset}>reset</button>
-			{/* <button onClick={filped}>filped</button> */}
+			<MemorizeBtn text={btn} onChangeText={onChangeText} />
+			<MemorizeBtn text="1231" />
 		</div>
 	);
-}
-ReactDOM.render(<Container />, root);
+};
+
+ReactDOM.render(<App />, root);
